1c6d29edc265d1e1fc23c8c0d254e9aa427d371d
christiannunez5 <christiannunez775@gmail.com>
2024-11-10 19:04:53 +0800

    feat: add thread card

--- client/src/components/layouts/layout.tsx
+++ client/src/components/layouts/layout.tsx
@@ -5,14 +5,17 @@ import { RightSidebar } from "@/components/shared/right-sidebar";
 
 export const MainLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="pt-20">
+    <div className="pt-[5.5rem]">
       <Navbar />
 
-      <main className="flex flex-col bg-background p-6 gap-6">
+      <main
+        className="bg-background flex-1 flex flex-col p-6 gap-6"
+        style={{ minHeight: "calc(100vh - 5.5rem)" }}
+      >
         <LeftSidebar />
 
         <div
-          className="
+          className="bg-background border-3 border-black
         md:mr-[17.5rem] lg:mx-[17.5rem]
         "
         >
---
+++ client/src/features/thread/components/thread-card.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
+
+export const ThreadCard = () => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  return (
+    <div
+      className="w-full bg-primary rounded-xl p-5 flex gap-5 relative flex-col
+    xl:flex-row"
+    >
+      <div
+        className={`absolute top-5 right-5 bg-[#EFF5F8] h-8 w-8 cursor-pointer
+          rounded-full grid place-content-center ${isLiked ? "text-accent" : "text-[#E0E0E0]"}`}
+        onClick={() => setIsLiked(!isLiked)}
+      >
+        <FaHeart className="text-lg" />
+      </div>
+
+      <img
+        className="border border-black rounded-xl flex-shrink-0 w-full mt-10
+            xl:w-[200px] xl:mt-0"
+        src="https://media.sproutsocial.com/uploads/2017/01/Instagram-Post-Ideas.png"
+        alt=""
+      />
+
+      <div className="w-full gap-10 flex flex-col">
+        <div className="space-y-4 text-xl">
+          <p>Learn machine learning 2024</p>
+          <div className="flex gap-2 text-xs text-accent-foreground font-light">
+            {/* tags */}
+            <div className="bg-accent py-2 px-3 rounded-2xl">finance</div>
+            <div className="bg-accent py-2 px-3 rounded-2xl">finance</div>
+            <div className="bg-accent py-2 px-3 rounded-2xl">finance</div>
+          </div>
+        </div>
+
+        <div className="w-full flex text-[#808080] items-center font-light justify-between">
+          <div className="flex gap-2 text-sm">
+            <div className="h-10 w-10 rounded-full border border-black flex-shrink-0"></div>
+            <div className="">
+              <p className="text-primary-foreground">Pavel Gray</p>
+              <p className="font-light text-xs">3 Weeks ago</p>
+            </div>
+          </div>
+
+          <div className="flex gap-3 text-sm">
+            <p className="cursor-pointer">652,324 views</p>
+
+            <p className="cursor-pointer">36,654 likes</p>
+
+            <p className="cursor-pointer">56 comments</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
